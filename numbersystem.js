e624b557e095240dcdec2d13f79b9c6e4e096417
namesearl <163240214+namesearl@users.noreply.github.com>
2024-04-01 04:12:33 +0800

    Update numbersystem.js

--- numbersystem.js
+++ numbersystem.js
@@ -3,17 +3,20 @@ let decInp = document.getElementById("decimalInp");
 function decToBin(decInp) {
   let y = parseInt(decInp.value);
   let x = y.toString(2);
+  x = isNaN(x) ? 0 : x;
   document.getElementById('dispbin').innerHTML = x;
 }
 
 function decToOct(decInp) {
   let y = parseInt(decInp.value);
   let x = y.toString(8);
+  x = isNaN(x) ? 0 : x;
   document.getElementById('dispoct').innerHTML = x;
 }
 
 function decToHex(decInp) {
   let y = parseInt(decInp.value);
   let x = y.toString(16);
+  x = isNaN(x) ? 0 : x;
   document.getElementById('disphex').innerHTML = x;
 }
